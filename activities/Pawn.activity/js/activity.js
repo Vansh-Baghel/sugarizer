304c31a9750fd389e2b7a46116127badf103bc39
Vansh Baghel <vanshbaghel912@gmail.com>
2023-03-03 11:50:16 +0530

    Step 1 towards contribution
    * Sugarizer Vanilla Javascript activity development tutorial 1 and tutorial 2 completed!

---
+++ activities/Pawn.activity/js/activity.js
@@ -0,0 +1,16 @@
+define(["sugar-web/activity/activity", "sugar-web/env"], function (
+  activity,
+  env
+) {
+  // Manipulate the DOM only when it is ready.
+  requirejs(["domReady!"], function (doc) {
+    // Initialize the activity.
+    activity.setup();
+
+    // Welcome user
+    env.getEnvironment(function (err, environment) {
+      document.getElementById("user").innerHTML =
+        "<h1>" + "Hello" + " " + environment.user.name + " !</h1>";
+    });
+  });
+});
